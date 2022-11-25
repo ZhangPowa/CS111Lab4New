6d832974e02c3ddb7bc14ee8c98b29a060d5413c
ZhangPowa <78633730+ZhangPowa@users.noreply.github.com>
2022-11-25 14:29:21 -0800

    Update ext2-create.c

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -303,7 +303,7 @@ void write_block_bitmap(int fd)
 		errno_exit("lseek");
 	}
 	ssize_t size = BLOCK_SIZE;
-	if (write(fd, &NUM_FREE_BLOCKS, size) != size)
+	if (write(fd, NUM_FREE_BLOCKS, size) != size)
 	{
 		errno_exit("write");
 	}
